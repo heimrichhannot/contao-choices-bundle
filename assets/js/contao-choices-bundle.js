d84488d111c2f15a9baca38a5ce2fd9971f10cb8
Eric Gesemann <e.gesemann@heimrich-hannot.de>
2024-01-31 11:20:33 +0100

    fixed options not generating

--- assets/js/contao-choices-bundle.js
+++ assets/js/contao-choices-bundle.js
@@ -4,59 +4,58 @@ class ChoicesBundle
 {
     static init()
     {
-        if (!ChoicesBundle.hasOwnProperty('choiceInstances'))
-        {
+        if (!ChoicesBundle.hasOwnProperty('choiceInstances')) {
             ChoicesBundle.choiceInstances = [];
         }
+
         let elements = document.querySelectorAll('[data-choices="1"]');
-        if (elements.length < 1)
-        {
+        if (elements.length < 1) {
             return;
         }
+
         elements.forEach((element) => {
-            let state = element.getAttribute('data-choice');
-            if('active' !== state) {
-                let defaultOptions = {};
-                let options = {};
-                if (element.hasAttribute('data-choices-options')) {
-                    options = JSON.parse(element.getAttribute('data-choices-options'));
-                    if (options.hasOwnProperty('addItemTextString'))
-                    {
-                        options.addItemText = (value) => {
-                            return options.addItemTextString;
-                        };
-                        delete options.addItemTextString;
-                    }
-                    if (options.hasOwnProperty('maxItemTextString'))
-                    {
-                        options.maxItemText = (maxItemCount) => {
-                            return options.maxItemTextString;
-                        };
-                        delete options.maxItemTextString;
-                    }
-                    options = Object.assign(defaultOptions, options);
+            const state = element.getAttribute('data-choice');
+            if (state === 'active') {
+                return;
+            }
+
+            let options = {};
+
+            const jsonChoicesOptions = element.getAttribute('data-choices-options');
+            if (jsonChoicesOptions !== null && jsonChoicesOptions !== '')
+            {
+                options = JSON.parse(jsonChoicesOptions);
+                const { addItemTextString, maxItemTextString } = options;
+
+                if (addItemTextString !== undefined) {
+                    options.addItemText = _ => addItemTextString;
+                    delete options.addItemTextString;
                 }
 
-                let optionsEvent = new CustomEvent('hundhChoicesOptions', {
-                    bubbles: true,
-                    detail: {
-                        options: options,
-                    },
-                });
-                element.dispatchEvent(optionsEvent);
-
-                let choice = new Choices(element, optionsEvent.detail.options);
-
-                let event = new CustomEvent('hundhChoicesNewInstance', {
-                    bubbles: true,
-                    detail: {
-                        instance: choice,
-                    },
-                });
-                element.dispatchEvent(event);
-
-                ChoicesBundle.choiceInstances.push({element: element, instance: choice});
+                if (maxItemTextString !== undefined) {
+                    options.maxItemText = _ => maxItemTextString;
+                    delete options.maxItemTextString;
+                }
             }
+
+            let optionsEvent = new CustomEvent('hundhChoicesOptions', {
+                bubbles: true,
+                detail: {
+                    options: options,
+                },
+            });
+            element.dispatchEvent(optionsEvent);
+
+            const choices = new Choices(element, optionsEvent.detail.options);
+            const newInstanceEvent = new CustomEvent('hundhChoicesNewInstance', {
+                bubbles: true,
+                detail: {
+                    instance: choices,
+                },
+            });
+            element.dispatchEvent(newInstanceEvent);
+
+            ChoicesBundle.choiceInstances.push({element: element, instance: choices});
         });
     }
 
